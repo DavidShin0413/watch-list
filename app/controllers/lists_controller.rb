d637cde1a221c04fe01c21007093b7ddf3bd761f
Kyung Ho Shin <davidshin0413@gmail.com>
2021-11-11 16:54:44 -0500

    almost done with back-end stuff

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,10 +5,31 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def new
     @list = List.new
   end
 
+  def create
+    @list = List.new(list_params)
+    @list.save
+
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to lists_path
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
