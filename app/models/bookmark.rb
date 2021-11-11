34f83e57c72cd39d35bd3b515e3cf05d7e3a9b7a
Kyung Ho Shin <davidshin0413@gmail.com>
2021-11-11 15:03:53 -0500

    committing

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
-  validates
+  validates :movie_id, uniqueness: { scope: :list_id }
   validates :comment, length: { minimum: 6 }
 end
